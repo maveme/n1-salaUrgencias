5d5e42413289e9d20d144581fcb8140697eeefda
maveme <help@genmymodel.com>
2016-05-23 21:27:36 +0000

    commit by maveme using genmymodel: v20160523_212735

---
+++ src-spec/uniandes/cupi2/model/interfaz/spec/InterfazSalaUrgencias.java
@@ -0,0 +1,224 @@
+package uniandes.cupi2.model.interfaz.spec;
+
+import co.edu.uniandes.cupi2.generador.anotaciones.interfaz.*;
+import uniandes.cupi2.model.mundo.*;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+
+/**
+ * Ventana principal de la aplicación.
+ */
+@UIFrame(alto=800, ancho=600, titulo = "SalaUrgencias")
+public class InterfazSalaUrgencias extends _InterfazSalaUrgencias
+{
+
+	/**
+	* <span style="font-size: 12.8px;">Constante que representa el área 2.</span>
+	*/
+	public final static Integer AREA_2 = 2;
+	/**
+	* <span style="font-size: 12.8px;">Constante que representa el área 3.</span>
+	*/
+	public final static Integer AREA_3 = 3;
+	/**
+	* Constante que representa el área 1.
+	*/
+	public final static Integer AREA_1 = 1;
+	/**
+	* <span style="font-size: 12.8px;">Constante que representa el área 4.</span>
+	*/
+	public final static invalid AREA_4 = 4;
+	
+
+	/**
+	* <span style="font-size: 12.8px;">Constante que representa el área 2.</span>
+	*/
+	private Integer AREA_2=2; 	
+	
+	/**
+	* <span style="font-size: 12.8px;">Constante que representa el área 3.</span>
+	*/
+	private Integer AREA_3=3; 	
+	
+	/**
+	* Constante que representa el área 1.
+	*/
+	private Integer AREA_1=1; 	
+	
+	/**
+	* <span style="font-size: 12.8px;">Constante que representa el área 4.</span>
+	*/
+	private invalid AREA_4=4; 	
+	
+	
+
+	/**
+	* 
+	*/
+	private PanelArea panelArea4;
+	
+	/**
+	* 
+	*/
+	private PanelOpciones panelOpciones;
+	
+	/**
+	* 
+	*/
+	private PanelArea panelArea1;
+	
+	/**
+	* 
+	*/
+	private PanelArea panelArea2;
+	
+	/**
+	* 
+	*/
+	private SalaUrgencias salaUrgencias;
+	
+	/**
+	* 
+	*/
+	private PanelImagen panelImagen;
+	
+	/**
+	* 
+	*/
+	private PanelInformacion panelInformacion;
+	
+	/**
+	* 
+	*/
+	private PanelArea panelArea3;
+	
+	
+
+	/**
+	* 
+	*/
+	public InterfazSalaUrgencias()
+	 {
+
+	 }
+
+
+	
+	/**
+	* 
+	*/
+	public void retirarProfesionales(Integer pNumeroArea)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void reqFuncIocion2()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void ingresarPacientes(Integer pNumeroArea)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void retirarPacientes(invalid pNumeroArea)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void reFuncOpcion1()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void reiniciar()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void agregarProfesionales(Integer pNumeroArea)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void actualizarPaneles()
+	{
+	
+	}
+	
+	
+
+
+
+    // -----------------------------------------------------------------
+    // Puntos de Extensión
+    // -----------------------------------------------------------------
+
+
+    /**
+     * Método para la extensión 1
+     */
+    public void reqFuncOpcion1( )
+    {
+        String resultado = salaUrgencias.metodo1();
+        JOptionPane.showMessageDialog( this, resultado, "Respuesta", JOptionPane.INFORMATION_MESSAGE );
+    }
+
+    /**
+     * Método para la extensión 2
+     */
+    public void reqFuncOpcion2( )
+    {
+        String resultado = salaUrgencias.metodo2();
+        JOptionPane.showMessageDialog( this, resultado, "Respuesta", JOptionPane.INFORMATION_MESSAGE );
+    }
+
+
+
+	// -----------------------------------------------------------------
+    // Main
+    // -----------------------------------------------------------------
+
+    /**
+    * Este método ejecuta la aplicación, creando una nueva interfaz.
+    * @param pArgs Argumentos para la ejecución de la aplicación. En este caso no son necesarios.
+    */
+    public static void main( String[] pArgs )
+    {
+    	// Unifica la interfaz para Mac y para Windows.
+    	try 
+    	{
+			UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName( ) );
+		} 
+    	catch (Exception e) 
+    	{
+			e.printStackTrace();
+		}
+    	
+        InterfazSalaUrgencias interfaz = new InterfazSalaUrgencias( );
+        interfaz.setVisible( true );
+    }
+}
---
+++ src-spec/uniandes/cupi2/model/mundo/spec/SalaUrgencias.java
@@ -0,0 +1,214 @@
+package uniandes.cupi2.model.mundo.spec;
+
+import co.edu.uniandes.cupi2.generador.anotaciones.*;
+
+/**
+* 
+*/
+@Mundo
+public class SalaUrgencias extends _SalaUrgencias
+{
+	
+
+	
+
+	/**
+	* 
+	*/
+	private AreaAtencion area4;
+	
+	/**
+	* 
+	*/
+	private AreaAtencion area2;
+	
+	/**
+	* 
+	*/
+	private AreaAtencion area3;
+	
+	/**
+	* 
+	*/
+	private AreaAtencion area1;
+	
+	
+
+	/**
+	* 
+	*/
+	public void retirarPacientesArea2(Integer pCantidadPacientes)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public Double darPorcentajeOcupacion()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void retirarPacientesArea3(Integer pCantidadPacientes)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void retirarPacientesArea4(Integer pCantidadPacientes)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void agregarProfesionalesArea1(Integer pCantidadProfesionales)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public Double darPorcentajeDisponibilidad()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void retirarProfesionalesArea2(Integer pCantidadProfesionales)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void ingresarPacientesArea3(Integer pCantidadPacientes)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void ingresarPacientesArea4(Integer pCantidadPacientes)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public Integer darTotalPacientes()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void ingresarPacientesArea1(Integer pCantidadPacientes)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void retirarProfesionalesArea3(Integer pCantidadProfesionales)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void agregarProfesionalesArea3(Integer pCantidadProfesionales)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void inicializar()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void retirarProfesionalesArea1(Integer pCantidadProfesionales)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void agregarProfesionalesArea4(Integer pCantidadProfesionales)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public Integer darCantidadPromedioProfesionales()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void ingresarPacientesArea2(Integer pCantidadPacientes)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public Integer darTotalCuposLibres()
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void agregarProfesionalesArea2(Integer pCantidadProfesionales)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void retirarPacientesArea1(Integer pCantidadPacientes)
+	{
+	
+	}
+	
+	/**
+	* 
+	*/
+	public void retirarProfesionalesArea4(Integer pCantidadProfesionales)
+	{
+	
+	}
+	
+	
+}
