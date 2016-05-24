3bf2e4a7f3cf1f85d4feefe89609d43a53b0dbb1
Mauricio Verano <m.verano239@gmail.com>
2016-05-23 20:56:33 -0500

    l

---
+++ src-test/uniandes/cupi2/salaUrgencias/test/AreaAtencionTest.java
@@ -0,0 +1,181 @@
+
+
+package uniandes.cupi2.salaUrgencias.test;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Clase usada para verificar que los métodos de la clase AreaAtencion estén correctamente implementados.
+ */
+public class AreaAtencionTest{
+
+
+	/**
+ 	* Este método se ejecuta antes de cada método de prueba.
+ 	*/
+	@Before
+	public void setUp() throws Exception {
+	
+	
+	}
+	
+	/**
+ 	* Este método se ejecuta después de cada método de prueba.
+ 	*/
+	@After
+	public void tearDown() throws Exception {
+	
+	
+	}
+	
+	
+	/**
+     * Prueba 1 : verifica el método darNombre.<br>
+    */
+	@Test
+	public void testdarNombre() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 2 : verifica el método modificarNombre.<br>
+    */
+	@Test
+	public void testmodificarNombre() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 3 : verifica el método darCantidadProfesionales.<br>
+    */
+	@Test
+	public void testdarCantidadProfesionales() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 4 : verifica el método modificarCantidadProfesionales.<br>
+    */
+	@Test
+	public void testmodificarCantidadProfesionales() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 5 : verifica el método darRutaImagen.<br>
+    */
+	@Test
+	public void testdarRutaImagen() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 6 : verifica el método modificarRutaImagen.<br>
+    */
+	@Test
+	public void testmodificarRutaImagen() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 7 : verifica el método darCantidadPacientes.<br>
+    */
+	@Test
+	public void testdarCantidadPacientes() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 8 : verifica el método modificarCantidadPacientes.<br>
+    */
+	@Test
+	public void testmodificarCantidadPacientes() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 9 : verifica el método darCantidadCuposLibres.<br>
+    */
+	@Test
+	public void testdarCantidadCuposLibres() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 10 : verifica el método agregarProfesionales.<br>
+    */
+	@Test
+	public void testagregarProfesionales() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 11 : verifica el método reiniciar.<br>
+    */
+	@Test
+	public void testreiniciar() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 12 : verifica el método retirarProfesionales.<br>
+    */
+	@Test
+	public void testretirarProfesionales() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 13 : verifica el método ingresarPacientes.<br>
+    */
+	@Test
+	public void testingresarPacientes() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 14 : verifica el método darCantidadCuposDisponibles.<br>
+    */
+	@Test
+	public void testdarCantidadCuposDisponibles() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 15 : verifica el método retirarPacientes.<br>
+    */
+	@Test
+	public void testretirarPacientes() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 16 : verifica el método inicializar.<br>
+    */
+	@Test
+	public void testinicializar() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+
+}
---
+++ src-test/uniandes/cupi2/salaUrgencias/test/SalaUrgenciasTest.java
@@ -0,0 +1,325 @@
+
+
+package uniandes.cupi2.salaUrgencias.test;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Clase usada para verificar que los métodos de la clase SalaUrgencias estén correctamente implementados.
+ */
+public class SalaUrgenciasTest{
+
+
+	/**
+ 	* Este método se ejecuta antes de cada método de prueba.
+ 	*/
+	@Before
+	public void setUp() throws Exception {
+	
+	
+	}
+	
+	/**
+ 	* Este método se ejecuta después de cada método de prueba.
+ 	*/
+	@After
+	public void tearDown() throws Exception {
+	
+	
+	}
+	
+	
+	/**
+     * Prueba 1 : verifica el método darArea2.<br>
+    */
+	@Test
+	public void testdarArea2() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 2 : verifica el método modificarArea2.<br>
+    */
+	@Test
+	public void testmodificarArea2() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 3 : verifica el método darArea4.<br>
+    */
+	@Test
+	public void testdarArea4() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 4 : verifica el método modificarArea4.<br>
+    */
+	@Test
+	public void testmodificarArea4() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 5 : verifica el método darArea3.<br>
+    */
+	@Test
+	public void testdarArea3() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 6 : verifica el método modificarArea3.<br>
+    */
+	@Test
+	public void testmodificarArea3() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 7 : verifica el método darArea1.<br>
+    */
+	@Test
+	public void testdarArea1() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 8 : verifica el método modificarArea1.<br>
+    */
+	@Test
+	public void testmodificarArea1() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 9 : verifica el método darTotalPacientes.<br>
+    */
+	@Test
+	public void testdarTotalPacientes() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 10 : verifica el método retirarPacientesArea1.<br>
+    */
+	@Test
+	public void testretirarPacientesArea1() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 11 : verifica el método ingresarPacientesArea4.<br>
+    */
+	@Test
+	public void testingresarPacientesArea4() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 12 : verifica el método inicializar.<br>
+    */
+	@Test
+	public void testinicializar() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 13 : verifica el método darTotalCuposLibres.<br>
+    */
+	@Test
+	public void testdarTotalCuposLibres() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 14 : verifica el método retirarProfesionalesArea2.<br>
+    */
+	@Test
+	public void testretirarProfesionalesArea2() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 15 : verifica el método darCantidadPromedioProfesionales.<br>
+    */
+	@Test
+	public void testdarCantidadPromedioProfesionales() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 16 : verifica el método retirarPacientesArea3.<br>
+    */
+	@Test
+	public void testretirarPacientesArea3() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 17 : verifica el método ingresarPacientesArea1.<br>
+    */
+	@Test
+	public void testingresarPacientesArea1() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 18 : verifica el método agregarProfesionalesArea2.<br>
+    */
+	@Test
+	public void testagregarProfesionalesArea2() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 19 : verifica el método retirarPacientesArea2.<br>
+    */
+	@Test
+	public void testretirarPacientesArea2() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 20 : verifica el método ingresarPacientesArea2.<br>
+    */
+	@Test
+	public void testingresarPacientesArea2() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 21 : verifica el método retirarProfesionalesArea1.<br>
+    */
+	@Test
+	public void testretirarProfesionalesArea1() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 22 : verifica el método agregarProfesionalesArea4.<br>
+    */
+	@Test
+	public void testagregarProfesionalesArea4() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 23 : verifica el método darPorcentajeDisponibilidad.<br>
+    */
+	@Test
+	public void testdarPorcentajeDisponibilidad() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 24 : verifica el método darPorcentajeOcupacion.<br>
+    */
+	@Test
+	public void testdarPorcentajeOcupacion() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 25 : verifica el método retirarProfesionalesArea4.<br>
+    */
+	@Test
+	public void testretirarProfesionalesArea4() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 26 : verifica el método retirarProfesionalesArea3.<br>
+    */
+	@Test
+	public void testretirarProfesionalesArea3() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 27 : verifica el método agregarProfesionalesArea1.<br>
+    */
+	@Test
+	public void testagregarProfesionalesArea1() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 28 : verifica el método retirarPacientesArea4.<br>
+    */
+	@Test
+	public void testretirarPacientesArea4() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 29 : verifica el método agregarProfesionalesArea3.<br>
+    */
+	@Test
+	public void testagregarProfesionalesArea3() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 30 : verifica el método ingresarPacientesArea3.<br>
+    */
+	@Test
+	public void testingresarPacientesArea3() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 31 : verifica el método metodo1.<br>
+    */
+	@Test
+	public void testmetodo1() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+	/**
+     * Prueba 32 : verifica el método metodo2.<br>
+    */
+	@Test
+	public void testmetodo2() {
+	
+		fail("No se ha implementado la prueba");
+		
+	}
+
+}
